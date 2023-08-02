37aec11d0b5078c0115bd4750227e6713404e90a
Mariam Ashmawi <mariam18@uw.edu>
2023-08-01 20:32:36 -0700

    added new demo html and js file

---
+++ lesson04/demo.js
@@ -0,0 +1,10 @@
+function numberAdder() {
+    let input = prompt('Enter a number');
+    let i = Number(input);
+    let numberToSquare = i;
+    let result = 0; 
+    
+    result = numberToSquare * Number(input);
+    
+    alert(result);    
+}
